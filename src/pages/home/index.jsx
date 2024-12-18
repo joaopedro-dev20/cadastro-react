48d1794d0eab07b48b3a555f1869d601eb188ad5
joaopedro-dev20 <joaocampos.archer@gmail.com>
2024-12-18 19:29:13 -0300

    Front end em React ligado ao Back end. 1° projeto concluído

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,18 @@
 import Trash from '../../asset/lixo.png'
 import './style.css'
 import api from '../../service/api'
-import { useEffect, useState } from 'react'
+import { useEffect, useState,useRef } from 'react'
 // UseEffect para chamar a função GET como recomdado no REACT. 
 
-
+// adicionado um novo usuário com useRef pela interface do Front em React. 
 function Home() {
 
   const [users, setUsers] = useState([])
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
 
   //UseState permite que quando houver alterações consigamos visualizá-las. 
 
@@ -17,8 +22,31 @@ function Home() {
     setUsers(usersFromApi.data)
   }
 
-  useEffect(() => {
+  // O create users é chamado ao clique do botão. Por isso o onClick={createUsers} por lá.
+  // o axios permite procurar as infos por essa forma. Descrevendo exatamente e adicionando o caminho de onde estará as infos inseridas.
+  async function createUsers() {
+    
+    await api.post('/usuarios', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    })
+   
     getUsers()
+  }
+
+  async function deleteUsers(id) {
+
+    //Para deletar vou deletar o usuário pelo ID. Utilizei a `` para encaixar a variável junto com a string.
+    await api.delete(`/usuarios/${id}`)
+
+    getUsers()
+  }
+
+  useEffect(() => {
+
+    getUsers()
+
   }, [])
 
   return (
@@ -26,10 +54,10 @@ function Home() {
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input placeholder='Nome' name='name' type='text' />
-        <input placeholder='Idade' name='idade' type='number' />
-        <input placeholder='Email' name='email' type='email' />
-        <button type='button'>Cadastrar</button>
+        <input placeholder='Nome' name='name' type='text' ref={inputName}/>
+        <input placeholder='Idade' name='idade' type='number' ref={inputAge}/>
+        <input placeholder='Email' name='email' type='email' ref={inputEmail}/>
+        <button type='button' onClick={createUsers}>Cadastrar</button>
       </form>
 
       {users.map((user) => (
@@ -40,8 +68,8 @@ function Home() {
             <p>Idade: <span>{user.age}</span></p>
             <p>Email: <span>{user.email}</span></p>
           </div>
-          <button>
-            <img src={Trash}
+          <button onClick={() => deleteUsers(user.id)}>
+            <img src={Trash}//acima, é necessário colocar o () => para que o REACT não trave. 
               alt="lata de lixo"
               width={50}
               height={50}

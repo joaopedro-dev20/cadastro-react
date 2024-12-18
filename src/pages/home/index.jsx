dc9969ffb4e7099f4c138ea876e93379b66d8f3b
joaopedro-dev20 <joaocampos.archer@gmail.com>
2024-12-18 16:39:47 -0300

    atualização do cadastro inserindo html dentro de js

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,42 +11,43 @@ function Home() {
       age: 24,
       email: 'joao@gmail.com',
 
-     },
-     {
+    },
+    {
 
       id: '2136egasdg',
       name: 'Rafa',
       age: 25,
       email: 'rafa@gmail.com'
 
-      },
-      ]
+    },
+  ]
 
-return (
+  return (
 
-  <div className='container'>
-    <form>
-      <h1>Cadastro de Usuários</h1>
-      <input name='name' type='text' />
-      <input name='idade' type='number' />
-      <input name='email' type='email' />
-      <button type='button'>Cadastrar</button>
-    </form>
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input name='name' type='text' />
+        <input name='idade' type='number' />
+        <input name='email' type='email' />
+        <button type='button'>Cadastrar</button>
+      </form>
 
-    <div>
-      <div>
-        <p>Nome:</p>
-        <p>Idade:</p>
-        <p>Email:</p>
-      </div>
-      <button>
-        <img src={Trash} alt="" />
-      </button>
+      {users.map((user) => (
+
+        <div key={user.id}>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Idade: {user.age}</p>
+            <p>Email: {user.email}</p>
+          </div>
+          <button>
+            <img src={Trash} alt="" />
+          </button>
+        </div>
+      ))}    
     </div>
-
-  </div>
-
-)
+  )
 }
 
-export default Home
+export default Home;
